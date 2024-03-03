d71db2f3ce7034e059067f3db29af2c8f950b8a1
Moiz Babinwale <mbabinwale@gmail.com>
2024-03-03 22:15:39 +0530

    api changed

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,2 +1,2 @@
-export const API = "http://localhost:5000/api";
-export const TEST_API = "http://localhost:5000/test";
+export const API = "https://practice-pro-server.vercel.app/api";
+export const TEST_API = "https://practice-pro-server.vercel.app/test";
